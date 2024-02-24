772b9822d3e4bf0f75795fc4dc229447a60d242d
leomark paway <leomarkpaway@gmail.com>
2024-02-24 12:20:27 -0800

    :hammer: Move todo repository on domain package

---
+++ app/src/main/java/com/leomarkpaway/todolist/domain/TodoRepository.kt
@@ -0,0 +1,9 @@
+package com.leomarkpaway.todolist.domain
+
+import androidx.lifecycle.LiveData
+import com.leomarkpaway.todolist.data.source.local.entity.Todo
+
+interface TodoRepository {
+    suspend fun addTodo(todo: Todo)
+    suspend fun getAllTodo() : LiveData<List<Todo>>
+}
