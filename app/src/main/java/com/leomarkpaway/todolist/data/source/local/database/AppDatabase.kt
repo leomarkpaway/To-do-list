1920ebff32824e974f2b8258cc93d3668d45ef00
leomark paway <leomarkpaway@gmail.com>
2024-02-24 12:15:16 -0800

    :hammer: Use room database util

--- app/src/main/java/com/leomarkpaway/todolist/data/source/local/database/AppDatabase.kt
+++ app/src/main/java/com/leomarkpaway/todolist/data/source/local/database/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.leomarkpaway.todolist.common.util.createRoomDataBase
 import com.leomarkpaway.todolist.data.source.local.dao.TodoDao
 import com.leomarkpaway.todolist.data.source.local.database.AppDatabase.Companion.VERSION_NUMBER
 import com.leomarkpaway.todolist.data.source.local.entity.Todo
@@ -24,11 +25,7 @@ abstract class AppDatabase: RoomDatabase() {
 
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "app_database"
-                ).build()
+                val instance = context.createRoomDataBase<AppDatabase>("database_name")
                 INSTANCE = instance
                 instance
             }
