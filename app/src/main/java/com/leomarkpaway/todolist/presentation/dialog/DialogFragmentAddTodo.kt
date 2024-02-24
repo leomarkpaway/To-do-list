bbd2a3db6bb2c88eb2661874cbee11d908f6812f
leomark paway <leomarkpaway@gmail.com>
2024-02-24 15:51:43 -0800

    :sparkles: Initial add todo functionality

--- app/src/main/java/com/leomarkpaway/todolist/presentation/dialog/DialogFragmentAddTodo.kt
+++ app/src/main/java/com/leomarkpaway/todolist/presentation/dialog/DialogFragmentAddTodo.kt
@@ -1,10 +1,12 @@
 package com.leomarkpaway.todolist.presentation.dialog
 
+import android.util.Log
 import androidx.fragment.app.activityViewModels
 import com.leomarkpaway.todolist.R
 import com.leomarkpaway.todolist.TodoApp
 import com.leomarkpaway.todolist.common.base.BaseDialogFragment
 import com.leomarkpaway.todolist.common.util.viewModelFactory
+import com.leomarkpaway.todolist.data.source.local.entity.Todo
 import com.leomarkpaway.todolist.databinding.DialogFragmentAddTodoBinding
 import com.leomarkpaway.todolist.presentation.TodoViewModel
 
@@ -16,10 +18,25 @@ class DialogFragmentAddTodo : BaseDialogFragment<TodoViewModel, DialogFragmentAd
 
     override fun initViews() {
         super.initViews()
+        onClickSubmit()
+        onCLickCancel()
     }
 
     override fun subscribe() {
         super.subscribe()
+    }
+
+    private fun onClickSubmit() = with(binding) {
+        val title = tiTitle.editText?.text
+        val description = edtDescription.text
+        btnSubmit.setOnClickListener {
+            viewModel.addTodo(Todo(null,title.toString(), description.toString(), "8:00 pm"))
+            dismiss()
+        }
+    }
+
+    private fun onCLickCancel() = with(binding) {
+        btnCancel.setOnClickListener { dismiss() }
     }
 
 }
